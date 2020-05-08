1dd170a630b5f1bd686401b748b64646cfb26989
Ralfe Reis <ralfe.cavalcante@gmail.com>
2020-05-08 20:26:20 -0300

    Starting the challenge

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -30,12 +30,12 @@ interface Balance {
 }
 
 const Dashboard: React.FC = () => {
-  // const [transactions, setTransactions] = useState<Transaction[]>([]);
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
   // const [balance, setBalance] = useState<Balance>({} as Balance);
 
   useEffect(() => {
     async function loadTransactions(): Promise<void> {
-      // TODO
+      // to do
     }
 
     loadTransactions();
